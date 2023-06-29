c64633c2ae63e32f5e59aa328783c5afb18a0dc1
Igor Achkas <jigoren@yandex.ru>
2023-06-29 22:27:10 +0700

    add 1

---
+++ Перегрузка операторов 9.1.cpp
@@ -0,0 +1,50 @@
+﻿// Перегрузка операторов 9.1
+
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;//числитель
+		denominator_ = denominator;// знаминатель
+	}
+
+	/*int get_priv_fieln() { return numerator_; }
+	int get_priv_field() { return denominator_; }*/	
+
+	int fract_c() {
+		return numerator_* denominator_;
+	}
+
+	bool operator==(Fraction other) { return fract_c() == other.fract_c(); }
+	bool operator!=(Fraction other) { return  !(*this == other); /*fract_c() != other.fract_c();*/ }
+	bool operator<(Fraction other) { return fract_c() > other.fract_c(); }
+	bool operator>(Fraction other) { return other < *this;/*return fract_c() > other.fract_c();*/ }
+	bool operator<=(Fraction other) { return !(*this > other);/*return fract_c() >= other.fract_c();*/ }
+	bool operator>=(Fraction other) { return !(*this < other);/*return fract_c() <= other.fract_c();*/ }	
+};
+
+
+int main()
+{	
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);	
+	
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;    
+}
+
+
+
+
